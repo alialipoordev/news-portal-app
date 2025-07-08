4a71b506be5b9f6f3a2cb951b09f5b63af25f0c0
Ali Alipoor <alialipoordev@gmail.com>
2025-07-09 01:26:25 +0330

    fix: CORS config

--- server.js
+++ server.js
@@ -15,16 +15,31 @@ dotenv.config();
 // middleware
 app.use(bodyParser.json());
 
-const corsOptions =
-  process.env.NODE_ENV === "production"
-    ? cors({
-        origin: [process.env.VERCEL_ORIGIN, process.env.VERCEL_ORIGIN_CLIENT],
-      })
-    : cors({
-        origin: ["http://localhost:5173", "http://localhost:3000"],
-      });
+// const corsOptions =
+//   process.env.NODE_ENV === "production"
+//     ? cors({
+//         origin: [process.env.VERCEL_ORIGIN, process.env.VERCEL_ORIGIN_CLIENT],
+//       })
+//     : cors({
+//         origin: ["http://localhost:5173", "http://localhost:3000"],
+//       });
+// app.use(corsOptions);
 
-app.use(corsOptions);
+const corsOptions = {
+  origin: [
+    "https://easynews24.vercel.app",
+    "https://easynews24.vercel.app/client",
+    "http://localhost:5173",
+    "http://localhost:3000",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 const port = process.env.PORT;
 
